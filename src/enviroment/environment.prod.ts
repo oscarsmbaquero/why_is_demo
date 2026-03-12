7ced1328a2c15a650ee6985e16ab81f5a44ad9c0
oscarsmbaquero <oscarsmbaquero@gmail.com>
2026-03-12 22:09:06 +0100

    otra prueba mas

--- src/enviroment/environment.prod.ts
+++ src/enviroment/environment.prod.ts
@@ -1,10 +1,12 @@
 export const environment = {
     production: true,
     apiUrl: '/api/',
+    apiUrlLogin: 'https://why-back-node.vercel.app',
     apiUrlN8n: '/api/',
+    encriptKey: 'encrypt!135790',
     emailjs: {
-      serviceId: 'YOUR_SERVICE_ID',
-      templateId: 'YOUR_TEMPLATE_ID',
-      publicKey: 'YOUR_PUBLIC_KEY'
+      serviceId: 'service_gt0nj0d',
+      templateId: 'template_ykkxdec',
+      publicKey: 'MFnsya2QAXWHvl6IZ'
     }
   };
