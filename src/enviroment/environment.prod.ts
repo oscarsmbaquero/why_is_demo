3d1a1d483c4c298228bbef194b60780f5a0cff85
oscarsmbaquero <oscarsmbaquero@gmail.com>
2026-03-13 07:42:06 +0100

    otra prueba

--- src/enviroment/environment.prod.ts
+++ src/enviroment/environment.prod.ts
@@ -3,7 +3,6 @@ export const environment = {
     apiUrl: 'https://abolitionary-verline-erethismic.ngrok-free.dev/webhook/api/chat',
     n8nApiUrl: '/n8n-api',
     apiUrlLogin: 'https://why-back-node.vercel.app',
-    apiUrlN8n: '/api/',
     encriptKey: 'encrypt!135790',
     emailjs: {
       serviceId: 'service_gt0nj0d',
